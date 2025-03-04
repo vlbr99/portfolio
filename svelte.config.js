cb0b69d40f24e94d6eaad5d81d33c62bae8f9aef
vlbr99 <veliborminic99@gmail.com>
2025-03-04 18:22:40 +0100

    portfolio v1.0

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import adapter from "@sveltejs/adapter-static";
 export default {
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       pages: "build",
       assets: "build",
       fallback: undefined,
